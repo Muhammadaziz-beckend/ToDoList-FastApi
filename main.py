2ddcd2f39e596c18f8625d7ae07fc66711a77c06
Muhammadaziz <muhammadazizdev@gmail.com>
2025-02-02 19:57:22 +0600

    run fastApi

--- main.py
+++ main.py
@@ -15,3 +15,5 @@ if __name__ == "__main__":
         uvicorn.run(app, port=8000, host="127.0.0.1")
     except Exception as e:
         print("Ошибка в программе", e)
+
+# run uvicorn main:app --reload
