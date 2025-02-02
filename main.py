d2cc4e9f30d32adabb5375d29fcaccf6df5a290d
Muhammadaziz <muhammadazizdev@gmail.com>
2025-02-02 19:56:24 +0600

    print hello world

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+
+    return "Hello World"
+
+if __name__ == "__main__":
+
+    try:
+        uvicorn.run(app, port=8000, host="127.0.0.1")
+    except Exception as e:
+        print("Ошибка в программе", e)
